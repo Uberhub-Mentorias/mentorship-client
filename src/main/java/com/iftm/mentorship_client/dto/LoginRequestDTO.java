863fe4a1e7d6d4d8f17bcf15003d5243e32ea74c
arturmsoares <arturms16@gmail.com>
2025-11-28 16:58:24 -0300

    inserindo os dtos de auth-service

---
+++ src/main/java/com/iftm/mentorship_client/dto/LoginRequestDTO.java
@@ -0,0 +1,22 @@
+package com.iftm.mentorship_client.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDTO {
+
+    @NotBlank(message = "O e-mail é obrigatório")
+    @Email(message = "Formato de e-mail inválido")
+    private String email;
+
+    @NotBlank(message = "A senha é obrigatória")
+    private String password;
+}
---
+++ src/main/java/com/iftm/mentorship_client/model/enums/Role.java
@@ -0,0 +1,7 @@
+package com.iftm.mentorship_client.model.enums;
+
+public enum Role {
+    MENTOR,
+    MENTEE,
+    ADMIN
+}
